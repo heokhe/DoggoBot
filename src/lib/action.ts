606e2504674c1b192eae2d872754b5f355602ea1
Hosein Khansari <hrk.x11@gmail.com>
2020-08-22 08:06:11 +0430

    Convert `Direction` to a const enum

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -1,19 +1,9 @@
-export enum Direction {
+export const enum Direction {
   North = 1,
   East,
   South,
   West
 }
-
-// How about:
-// export enum Direction3D {
-//   North,
-//   East,
-//   South,
-//   West,
-//   Front,
-//   Back
-// }
 
 export class Action {
   direction: Direction;
