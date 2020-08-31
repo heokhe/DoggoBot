8c4a5459ef4036f23a8dcaaf91fe4969399f7f56
Hosein Khansari <hrk.x11@gmail.com>
2020-08-31 17:42:21 +0430

    Complete the visualization logic in the dev page

--- src/public/app.js
+++ src/public/app.js
@@ -1,35 +1,57 @@
 const fakeUnityClient = new WebSocket('ws://localhost:8000');
 const server = new WebSocket('ws://localhost:8000?dev');
-const root = document.getElementById('table');
+const root = document.querySelector('main');
 
 fakeUnityClient.onopen = () => {
   fakeUnityClient.send('config 3 1 5');
 };
 
-function render(table) {
-  root.innerHTML = '';
+// eslint-disable-next-line one-var-declaration-per-line
+let min, max, table;
+
+const calculateColor = value => {
+  const base = value > 0 ? '78,178,78' : '214,80,74';
+  const opacity = value / (value > 0 ? max : min);
+  return `rgb(${base}, ${opacity})`;
+};
+
+function setupAndRender() {
   root.style.gridTemplateColumns = `repeat(${table.width}, 1fr)`;
-  for (const cell of table.cells) {
-    const cellValue = cell.actions.map(a => a.value).reduce((a, b) => a + b, 0) / cell.actions.length;
-    root.insertAdjacentHTML('beforeend', `<div class="cell" title="(${cell.coordinates.x}, ${cell.coordinates.y})">${cellValue.toFixed(2)}</div>`);
+  for (const { avgValue, coordinates: { x, y } } of table.cells) {
+    if (max === undefined || avgValue > max) max = avgValue;
+    if (min === undefined || avgValue < min) min = avgValue;
+
+    root.insertAdjacentHTML(
+      'beforeend', `<div class="cell" title="(${x}, ${y})"></div>`
+    );
   }
 }
 
-function updateCell(index, cellObject) {
-  const cellValue = cellObject.actions.map(a => a.value).reduce((a, b) => a + b, 0) / cellObject.actions.length;
-  document.querySelectorAll('.cell')[index].innerHTML = cellValue.toFixed(2);
+function update(index, cell) {
+  const els = document.querySelectorAll('.cell');
+  const cellValue = cell.avgValue;
+  if (cellValue < min) min = cellValue;
+  if (cellValue > max) max = cellValue;
+  table.cells[index] = cell;
+
+  if (min === cellValue || max === cellValue) {
+    for (let i = 0; i < table.cells.length; i++) {
+      els[i].style.backgroundColor = calculateColor(table.cells[i].avgValue);
+    }
+  } else {
+    // there's no change in max or min, so we don't need to update the other cells
+    els[index].style.backgroundColor = calculateColor(cellValue);
+  }
 }
 
-let i = 0;
 server.addEventListener('message', ev => {
   const data = JSON.parse(ev.data);
   if (data.type === 'table') {
-    const { table } = data;
-    render(table);
+    ({ table } = data);
+    setupAndRender();
     fakeUnityClient.send('reward 1');
-  } else if (data.type === 'update' && i < 50000) {
-    updateCell(data.index, data.cell);
+  } else if (data.type === 'update') {
+    update(data.index, data.cell);
     fakeUnityClient.send(`reward ${Math.ceil(Math.random() * 5)}`);
-    i++;
   }
 });
